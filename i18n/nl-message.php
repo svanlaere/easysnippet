92b1024d191872ea660481bab233727807db120a
svanlaere <svanlaere@users.noreply.github.com>
2016-07-24 20:43:45 +0200

    Update nl-message.php

--- i18n/nl-message.php
+++ i18n/nl-message.php
@@ -12,6 +12,7 @@ return array(
     'Add a snippet the easy way' => 'Snippers op de makkelijke manier toevoegen',
     'There are no snippets found' => 'Er zijn geen snippers gevonden',
     'Select a snippet' => 'Selecteer een snipper',
+    'Add again' => 'Herhaal toevoeging',
     'Easysnippet Appearance' => 'Easysnippet weergave',
     'Menu or buttons?' => 'Wil je een menu of knoppen?',
     'Use' => 'Gebruik',
