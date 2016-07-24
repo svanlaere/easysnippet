9e64a81b6a14397932e51e6ea368f4ea15d8bc40
Yuli Che <yCherkashin@gmail.com>
2016-07-24 05:28:15 +0300

    Remove unused __() message

--- i18n/en-message.php
+++ i18n/en-message.php
@@ -10,7 +10,6 @@ if (!defined('IN_CMS')) { exit(); }
 return array(
     'Easysnippet' => 'Easysnippet',
     'Add a snippet the easy way' => 'Add a snippet the easy way',
-    'Snippets alphabetically' => 'All your snippets alphabetically',
     'There are no snippets found' => 'No snippets were found',
     'Select a snippet' => 'Choose a snippet',
     'Easysnippet Appearance' => 'Easysnippet Appearance',
