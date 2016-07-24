518d6380da8c566b281ad78c99d0a04be6e26ee7
Yuli Che <yCherkashin@gmail.com>
2016-07-24 22:08:31 +0300

    Fix unsaved changes prompt not firing

--- views/buttons.php
+++ views/buttons.php
@@ -35,7 +35,7 @@ $().ready(function() {
             <?php else: ?>
             return val + php_left + php_start + space + include_start + snippet + include_end + space + php_end + '\n';
             <?php endif; ?>
-        });
+        }).change();
 	return false
     };
 
