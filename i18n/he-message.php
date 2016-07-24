77857cb9a791e90525e0bb7cb46409321056db6d
Yuli Che <yCherkashin@gmail.com>
2016-07-24 22:38:15 +0300

    Add more i18n - Hebrew and Russian

---
+++ i18n/he-message.php
@@ -0,0 +1,21 @@
+<?php
+if (!defined('IN_CMS')) { exit(); }
+
+/**
+ * Easysnippet
+ * Hebrew i18n
+ *
+ */
+
+return array(
+    'Easysnippet' => 'איזי-סניפט',
+    'Add a snippet the easy way' => 'הוספת קטאים בקלות מתפרית',
+    'There are no snippets found' => 'לא נימצאו קטיי Snippet',
+    'Select a snippet' => 'בחר קטא Snippet',
+    'Add again' => 'הוסף עוד פעם',
+    'Easysnippet Appearance' => 'הגדרות תפרית Easysnippet',
+    'Menu or buttons?' => 'קטיים ככפטורים או קרשימה?',
+    'Use' => 'הצג',
+    'Long menu' => 'תפרית עם רשימת קטאים',
+    'Snippet buttons' => 'כמות כפתורים לכל קטא Snippet',
+);
---
+++ i18n/ru-message.php
@@ -0,0 +1,21 @@
+<?php
+if (!defined('IN_CMS')) { exit(); }
+
+/**
+ * Easysnippet
+ * Russian i18n
+ *
+ */
+
+return array(
+    'Easysnippet' => 'Изи-сниппет',
+    'Add a snippet the easy way' => 'Выбрать фрагмент из простого меню фрагментов',
+    'There are no snippets found' => 'Фрагментов не найдено',
+    'Select a snippet' => 'Выберите фрагмент для вставки',
+    'Add again' => 'Добавить снова',
+    'Easysnippet Appearance' => 'Настройки меню Easysnippet',
+    'Menu or buttons?' => 'По кнопке на фрагмент, или же одно длинное меню?',
+    'Use' => 'Показывать',
+    'Long menu' => 'Меню всех фрагментов',
+    'Snippet buttons' => 'Кнопку для каждого фрагмента',
+);
