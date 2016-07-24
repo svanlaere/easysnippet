c019f6a6db63cc11aa031007c4693841cc0e2ecd
svanlaere <svanlaere@users.noreply.github.com>
2016-07-24 10:13:22 +0200

    Update nl-message.php
    Updated Dutch i18n

--- i18n/nl-message.php
+++ i18n/nl-message.php
@@ -10,6 +10,11 @@ if (!defined('IN_CMS')) { exit(); }
 return array(
     'Easysnippet' => 'Easysnippet',
     'Add a snippet the easy way' => 'Snippers op de makkelijke manier toevoegen',
-        'There are no snippets found' => 'Er zijn geen snippers gevonden',
-        'Select a snippet' => 'Selecteer een snipper',
+    'There are no snippets found' => 'Er zijn geen snippers gevonden',
+    'Select a snippet' => 'Selecteer een snipper',
+    'Easysnippet Appearance' => 'Easysnippet weergave',
+    'Menu or buttons?' => 'Wil je een menu of knoppen?',
+    'Use' => 'Gebruik',
+    'Long menu' => 'Menu',
+    'Snippet buttons' => 'Knoppen',	
 );
